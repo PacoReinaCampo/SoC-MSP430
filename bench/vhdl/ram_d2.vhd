7e08ed3265fa1515e20beea1e71b7a19f0f00e7c
PacoReinaCampo <frareicam@gmail.com>
2020-07-13 00:23:32 +0200

    new commit

--- bench/vhdl/ram_d2.vhd
+++ bench/vhdl/ram_d2.vhd
@@ -36,7 +36,7 @@
 --
 -- =============================================================================
 -- Author(s):
---   Francisco Javier Reina Campo <frareicam@gmail.com>
+-- *   Paco Reina Campo <pacoreinacampo@queenfield.tech>
 --
 
 library ieee;
