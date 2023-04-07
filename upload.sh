0d4ef701c8933888b20c4ad1453225aa8bfebe74
PacoReinaCampo <frareicam@gmail.com>
2023-04-08 01:07:39 +0200

    Working in SoC-MSP430 🔨

--- upload.sh
+++ upload.sh
@@ -42,6 +42,7 @@
 ##                                                                               ##
 ###################################################################################
 
+git submodule update --init --recursive --remote
 git add *
 git commit -m "Working in SoC-MSP430 🔨"
 git push origin master
